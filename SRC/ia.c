fe4e537608903ea3556dd54c710686173d01cc1e
KevinHivert <hivertkevin@gmail.com>
2014-02-11 00:17:55 +0100

    debut correction ia. Reste qu'elle retire pas les coups qu'elle à jouée.

--- SRC/ia.c
+++ SRC/ia.c
@@ -25,9 +25,7 @@ int ia_eval(Reversi *reversi, Player player, Pos *pos, int depth)
 {
 	int i, j, bestScore, score;
 	Pos played_pos;
-	Reversi *reversi_save = malloc(sizeof *reversi_save);
-	memcpy(reversi_save, reversi, sizeof *reversi);
-
+	Reversi *reversi_save = NULL;
 
 	/*if (game_over())
 	{
@@ -38,20 +36,23 @@ int ia_eval(Reversi *reversi, Player player, Pos *pos, int depth)
 	{
 		return ia_eval_grid(reversi, player);
 	}
+
+
+	reversi_save = malloc(sizeof *reversi_save);
+	memcpy(reversi_save, reversi, sizeof *reversi);
 	
 	if (depth % 2 == 0)
 	{
 		bestScore = INT_MIN;
-		for(j = 1; j <= REVERSI_SIZE; j++)
+		for(j = 1; j < REVERSI_SIZE; j++)
 		{
-			for(i = 1; i <= REVERSI_SIZE; i++)
+			for(i = 1; i < REVERSI_SIZE; i++)
 			{
 				played_pos.x = i;
 				played_pos.y = j;
 				if (0 == reversi_set_ia_move(reversi, player, &played_pos))
 				{
-					score = ia_eval(reversi, INV_PLAYER(player), pos, depth - 1);
-					memcpy(reversi, reversi_save, sizeof(Reversi));
+					score = ia_eval(reversi, INV_PLAYER(player), pos, depth + 1);
 					if (score > bestScore)
 					{
 						bestScore = score;
@@ -59,20 +60,20 @@ int ia_eval(Reversi *reversi, Player player, Pos *pos, int depth)
 						pos->y = played_pos.y;
 					}
 				}
+				memcpy(reversi, reversi_save, sizeof *reversi_save);
 			}
 		}
 	} else {
 		bestScore = INT_MAX;
-		for(j = 1; j <= REVERSI_SIZE; j++)
+		for(j = 1; j < REVERSI_SIZE; j++)
 		{
-			for(i = 1; i <= REVERSI_SIZE; i++)
+			for(i = 1; i < REVERSI_SIZE; i++)
 			{
 				played_pos.x = i;
 				played_pos.y = j;
 				if (0 == reversi_set_ia_move(reversi, player, &played_pos))
 				{
-					score = ia_eval(reversi, INV_PLAYER(player), pos, depth - 1);
-					memcpy(reversi, reversi_save, sizeof(Reversi));
+					score = ia_eval(reversi, INV_PLAYER(player), pos, depth + 1);
 					if (score < bestScore)
 					{
 						bestScore = score;
@@ -80,13 +81,12 @@ int ia_eval(Reversi *reversi, Player player, Pos *pos, int depth)
 						pos->y = played_pos.y;
 					}
 				}
+				memcpy(reversi, reversi_save, sizeof *reversi_save);
 			}
 		}
 	}
-/*
- * Ne passe pas ici.
-  */
-return 0;
+free(reversi_save);
+return bestScore;
 }
 
 int ia_eval_grid (Reversi *reversi, Player player)
@@ -94,9 +94,9 @@ int ia_eval_grid (Reversi *reversi, Player player)
 	int i, j, pos, n = 0;
   	Player player2 = INV_PLAYER(player);
 
-	for(j = 1; j <= REVERSI_SIZE; j++)
+	for(j = 1; j < REVERSI_SIZE; j++)
 	{
-		for(i = 1; i <= REVERSI_SIZE; i++)
+		for(i = 1; i < REVERSI_SIZE; i++)
 		{
 			pos = POS(i, j);
 			if (reversi->array[pos] == player)
--- SRC/main.c
+++ SRC/main.c
@@ -8,17 +8,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "reversi.h"
+#include "ia.h"
 
 int main(int argc, char *argv[])
 {
   Reversi *reversi;
   Player player = PLAYER_2;
-
+  Pos *pos = malloc(sizeof *pos);
+  
   (void)argc;
   (void)argv;
 
   reversi = reversi_new();
   reversi_print(reversi);
+  ia_eval(reversi, player, pos, 0);
 
   for(;;)
   {
